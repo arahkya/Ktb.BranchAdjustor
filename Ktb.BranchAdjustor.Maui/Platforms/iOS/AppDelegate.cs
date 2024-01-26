3f74bfa87384e92461782a1691443ebbcc0fd6c7
Arahk Yambubpah <arahkyambubpah@Arahks-MBP.ST-244F>
2024-01-26 11:33:29 +0700

    Initial Project

---
+++ Ktb.BranchAdjustor.Maui/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Ktb.BranchAdjustor.Maui;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
